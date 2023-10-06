ff8841c0dbf211bd4b40d3692f9ba5604c83f834
Alain Wafflard <alain.wafflard@gmail.com>
2023-10-06 21:17:42 +0200

    upload exo 2023/10/02

--- 11-base/banque/compte.py
+++ 11-base/banque/compte.py
@@ -3,7 +3,30 @@ class Compte:
     def __init__(self, titulaire):
         self.__titulaire = titulaire
         self.__solde = 0
-        self._brol = "zouave"
+
+    @property
+    def titulaire (self):
+        return self.__titulaire
+
+    @titulaire.setter
+    def titulaire(self, name):
+        # tests de validité ...
+        self.__titulaire = name
+
+    @property
+    def solde(self):
+        return self.__solde
+
+    def deposer(self, montant):
+        self.__solde += montant
+        return 'dépôt accepté'
+
+    def retirer(self, montant):
+        if montant > self.__solde:
+            return 'solde insuffisant'
+        self.__solde -= montant
+        return 'retrait accepté'
+
 
 
 if __name__ == "__main__":
